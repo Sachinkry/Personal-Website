01e7fa647dc55ccddd9dc3dd72b96825a088467d
Sachin <heysachinkry@gmail.com>
2023-02-03 13:00:47 +0530

    content-added

---
+++ data/projects.js
@@ -0,0 +1,18 @@
+const projects = 
+[
+    {
+        "name": "BookBeats",
+        "description": "a gpt-3 powered web-app which recommends songs that vibe with your favorite books.",
+        "url": "https://BookBeats.sachinkry.repl.co",
+    },{
+        "name": "Discord-faucet-bot",
+        "description": "a bot that can be used to send testnet tokens to discord users.",
+        "url": "https://github.com/Sachinkry/Discord-bots/tree/main/bot-1",
+    },{
+        "name": "MailGenie",
+        "description": "a gmail extension that uses GPT-3 to write emails based on user's inputs.",
+        "url": "https://github.com/Sachinkry/GPT-3-Email-writer-extension",
+    },
+]
+
+export default projects;
--- pages/index.js
+++ pages/index.js
@@ -1,57 +1,64 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-import link from 'next/link'
 import Navbar from '../components/Navbar'
+import projects from '../data/projects'
+
 export default function Home() {
+  
+  // recommend kro yaar you are my friend
   return (
-    <div className='bg- mt-10'>
+    <div className='bg- mt-5 max-w-2xl mx-auto'>
        <Head>
         <link rel="stylesheet" href="/styles.css" />
       </Head>
       <header className="bg-white ">
-        <img src='/pfp-sac.jpg' className=' mx-5 h-40 w-40 border-4 border--200 ' alt='pfp-sachin'/>
+        <img src='/pfp-sac.jpg' className=' mx-5 h-40 w-40 border-1 border--200 shadow-lg' alt='pfp-sachin'/>
         {/* <Navbar /> */}
 
       </header>
       {/* Main body */}
       <main>
 
-        <div className="container mx-auto mt-10 max-w-2xl  tracking-tight ">
-          <p className=" mx-4 text-xl text-gray-600 leading-tight">
-            Hi there 👋, This is <span className='font-bold'>Sachin</span>. Treasuring the fondness I developed for coding the prev year to create stuffs that are fun to build and useful. 
+        <div className="container mx-auto mt-10   tracking-tight ">
+          <p className=" mx-4 text-xl text-gray-600 leading-tight mt-5">
+            Hi there 👋, This is <span className='font-bold'>Sachin</span>. I treasure the fondness I developed for coding last year to build stuffs that are useful, and fun <span className='font-semibold'>&#91;</span>umm.. actually involves smashing my head on the keyboard & some 'aha' moments<span className='font-semibold'>&#93;</span>. 
           </p>
-          <br></br>
-          <p className='mx-4 text-xl text-gray-600 leading-tight'>
-            This year I would be exploring AI and ML stuffs like NLP, LLMs and others to build cool shits.
-          </p>
-          <br></br>
-          <p className='mx-4 text-xl text-gray-600 leading-tight'>
-            When I am not coding I enjoy <a href='https://www.goodreads.com/user/show/148280232?ref=nav_profile_l' className='underline'>reading</a>, listening to <a href='https://youtube.com/playlist?list=PLX3ti02JFmSGG71oOKr7N760J8QTHkjKy ' className='underline'>music</a>, watching <a href='https://zoro.to/home' className='underline'>animes</a> and <a href='https://twitter.com/SachinKry/status/1497952709890035712?s=20&t=wVn3EJJZOaLy-TjpWSjuNg' className='underline'>drawing</a> simple stuffs.
-          </p>
-          <br></br>
-          <p className='mx-4 text-xl text-gray-600 '>Here are some of my builds:</p>
-          
-          <p className='mx-4 text-xl text-gray-600 px-2 leading-tight'>
-            <a href='https://github.com/Sachinkry/Discord-bots/tree/main/bot-1' > • <span className='underline font-bold '>Discord-faucet-bot</span></a>
-            &#x2c; a bot that can be used to send testnet tokens to discord users.
-          </p>
-          <p className='mx-4 text-xl text-gray-600 px-2 leading-tight'>
-            <a href='https://github.com/Sachinkry/GPT-3-Email-writer-extension'> • <span className='underline font-bold color-blue'>MailGenie</span></a>
-            &#x2c; a gmail extension that uses GPT-3 to write emails based on user&apos;s inputs.
+          {/* <br></br> */}
+          <p className='mx-4 mt-5 text-xl text-gray-600 leading-tight'>
+            This year I am exploring AI and ML stuffs like NLP, LLMs and others to build cool shits.
           </p>
           
+          <p className='mx-4 mt-5 text-xl text-gray-600 leading-tight'>
+            When I am not coding I enjoy <a 
+            href='https://www.goodreads.com/user/show/148280232?ref=nav_profile_l' 
+            className='underline'>reading</a>, listening to <a href='https://youtube.com/playlist?list=PLX3ti02JFmSGG71oOKr7N760J8QTHkjKy ' className='underline'>music</a>, watching <a  className=''>animes</a> and <a 
+            href='https://twitter.com/SachinKry/status/1497952709890035712?s=20&t=wVn3EJJZOaLy-TjpWSjuNg' className='underline'>drawing</a> simple stuffs.
+          </p>
+          
+          {/* projects */}
+          <div className=''>
 
-          {/* contact and other links */}
-          
-          <br></br>
-          <p className='mx-4 text-xl text-gray-600 leading-tight'>
-            If you would like to get in touch with me or just wanna say &apos;how u doin...&apos;[Joey-Friends], you can reach out to me on the bird app <a href='https://twitter.com/SachinKry' className='underline font-semibold'>@SachinKry</a> or mail me: <a href='mailto:heysachinkry@gmail.com' className='underline font-semibold'>heysachinkry@gmail.com</a>
+              <p className='mx-4 mt-5 text-xl text-gray-600 '>Here are some of my recent builds:</p>
+              {projects.map((project, index) => {
+                return (
+                      <p className='mx-4 text-xl text-gray-600 px-2 leading-tight' index={index}>
+                        <a href={project.url} > • <span className='underline font-bold '>{project.name}</span></a>
+                        &#x2c; {project.description}
+                      </p>
+
+                )
+              })}
+              
+          </div>         
+
+          {/* contact and other links */}         
+          <p className='mx-4 mt-5 text-xl text-gray-600 leading-tight mb-4'>
+            If you would like to get in touch with me or just wanna say &apos;how u doin...&apos;[Joey-Friends],  reach out to me on the bird app <a href='https://twitter.com/SachinKry' className='underline font-semibold'>@SachinKry</a> or mail me: <a href='mailto:heysachinkry@gmail.com' className='underline font-semibold'>heysachinkry@gmail.com</a>
           </p>
         </div>
       </main>
       <footer className="bg-gray-100 py-5">
         <div className="container mx-auto">
-          <p className="text-center text-gray-600">Last Updated: 2 Jan, 2023 </p>
+          <p className="text-center text-gray-600">Last Updated: 3 Feb, 2023 </p>
         </div>
       </footer>
       
